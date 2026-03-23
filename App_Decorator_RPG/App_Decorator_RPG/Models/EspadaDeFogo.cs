80103528a8775a8f6e61d64c36191937f5fca5e2
Luna <luna.beatrizalves@gmail.com>
2026-03-23 15:27:39 -0300

    Adicionando atributos da EspadaDeFogo que estendem do Decorator - Luna

--- App_Decorator_RPG/App_Decorator_RPG/Models/EspadaDeFogo.cs
+++ App_Decorator_RPG/App_Decorator_RPG/Models/EspadaDeFogo.cs
@@ -1,6 +1,8 @@
 ﻿namespace App_Decorator_RPG;
 
-public class EspadaDeFogo
+public class EspadaDeFogo : EquipamentoDecorator
 {
-    
+    public EspadaDeFogo(IPersonagem p) : base(p) { }
+    public override int Ataque => _personagemDecorado.Ataque + 30;
+    public override string Descricao => _personagemDecorado.Descricao + "Ataque de Fogo";
 }
