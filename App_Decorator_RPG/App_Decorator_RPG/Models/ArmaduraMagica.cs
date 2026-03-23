9858aa6766dd52d947743a286c7b9ce61656fde3
Luna <luna.beatrizalves@gmail.com>
2026-03-23 15:25:32 -0300

    Adicionando atributos da ArmaduraMagica que estendem do Decorator - Luna

--- App_Decorator_RPG/App_Decorator_RPG/Models/ArmaduraMagica.cs
+++ App_Decorator_RPG/App_Decorator_RPG/Models/ArmaduraMagica.cs
@@ -1,6 +1,8 @@
 ﻿namespace App_Decorator_RPG;
 
-public class ArmaduraMagica
+public class ArmaduraMagica : EquipamentoDecorator
 {
-    
+    public ArmaduraMagica(IPersonagem p) : base(p) { }
+    public override int Ataque => _personagemDecorado.Ataque * 40;
+    public override string Descricao => _personagemDecorado.Descricao + "Defesa de Armadura Magica";
 }
