df13c312e1152646c3d3c00b8a2e882ed80ea426
Luna <luna.beatrizalves@gmail.com>
2026-03-23 15:18:35 -0300

    Adicionando os atributos do Decorator herdados de IPersonagem e criando uma class ArmaduraMagica - Luna

--- App_Decorator_RPG/App_Decorator_RPG/Models/EquipamentoDecorator.cs
+++ App_Decorator_RPG/App_Decorator_RPG/Models/EquipamentoDecorator.cs
@@ -1,6 +1,17 @@
 ﻿namespace App_Decorator_RPG;
 
-public class EquipamentoDecorator
+public abstract class EquipamentoDecorator : IPersonagem
 {
+    protected IPersonagem _personagemDecorado;
+
+    protected EquipamentoDecorator(IPersonagem personagem)
+    {
+        _personagemDecorado = personagem;
+    }
+    
+    public virtual string Nome => _personagemDecorado.Nome;
+    public virtual int Ataque => _personagemDecorado.Ataque;
+    public virtual int Defense => _personagemDecorado.Defense;
+    public virtual string Descricao => _personagemDecorado.Descricao;
     
 }
