8f8a0a6a6ad9c0f527a6aeb590a3799160be0004
Luna <luna.beatrizalves@gmail.com>
2026-03-23 15:01:52 -0300

    Adicionando os atributos de Guerreiro, que estendem de IPersonagem - Luna

--- App_Decorator_RPG/App_Decorator_RPG/Models/Guerreiro.cs
+++ App_Decorator_RPG/App_Decorator_RPG/Models/Guerreiro.cs
@@ -1,6 +1,10 @@
 ﻿namespace App_Decorator_RPG;
 
-public class Guerreiro
+public class Guerreiro : IPersonagem
 {
+    public string Nome => "Herbert";
+    public int Ataque => 10;
+    public int Defense => 7;
+    public string Descricao => "Um guerreiro principiante, que possue uma espada e escudo de madeira.";
     
 }
