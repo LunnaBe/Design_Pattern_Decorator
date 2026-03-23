f968e18799cb9202aab5f0d121118acd420bbdad
LunnaBe <luna.beatrizalves@gmail.com>
2026-03-23 14:33:17 -0300

    Adicionando projeto WPF do Decorator - Luna

---
+++ App_Decorator_RPG/App_Decorator_RPG/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace App_Decorator_RPG;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
